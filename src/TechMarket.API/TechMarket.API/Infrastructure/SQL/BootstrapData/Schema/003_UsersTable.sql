8d1c243c4fcd13dde796afcb06f00b6242e96861
Yurii Borys <55096348+Yurii-Borys@users.noreply.github.com>
2021-06-25 13:21:07 +0300

    Added SQL scripts for DB creation. (#7)

---
+++ src/TechMarket.API/TechMarket.API/Infrastructure/SQL/BootstrapData/Schema/003_UsersTable.sql
@@ -0,0 +1,31 @@
+--============================================================================
+USE [TechMarket];
+GO
+------------------------------------------------------------------------------
+IF EXISTS(
+	SELECT *
+	FROM sys.tables
+	WHERE name = 'Users'
+		AND SCHEMA_NAME(schema_id) = 'dbo')
+	BEGIN
+		DROP TABLE dbo.Users;
+	END
+GO
+------------------------------------------------------------------------------
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_PADDING ON;
+GO
+--============================================================================
+CREATE TABLE dbo.Users (
+	[Id] INT NOT NULL IDENTITY(1, 1)
+		CONSTRAINT PK_Users PRIMARY KEY,
+	[FirstName] NVARCHAR(20) NOT NULL,
+	[LastName] NVARCHAR(20) NOT NULL,
+	[RoleId] INT NOT NULL
+		CONSTRAINT FK_Users_UserRoles REFERENCES dbo.UserRoles([Id]),
+	[Email] NVARCHAR(40) NOT NULL,
+	[Pasword] NVARCHAR(100) NOT NULL
+);
+GO
+--============================================================================
