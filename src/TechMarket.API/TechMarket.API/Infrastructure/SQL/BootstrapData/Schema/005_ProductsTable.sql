3100618088337c1c1cb459ce7813c03c6be439d6
morphineForOdin <svyatoslav.malinovskyj@gmail.com>
2021-07-05 13:33:56 +0300

    Moved DB connection string to the app settings.

--- src/TechMarket.API/TechMarket.API/Infrastructure/SQL/BootstrapData/Schema/005_ProductsTable.sql
+++ src/TechMarket.API/TechMarket.API/Infrastructure/SQL/BootstrapData/Schema/005_ProductsTable.sql
@@ -20,12 +20,12 @@ GO
 CREATE TABLE dbo.Products (
 	[Id] INT NOT NULL IDENTITY(1, 1)
 		CONSTRAINT PK_Products PRIMARY KEY,
-	[Name] NVARCHAR(30) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
 	[Description] NVARCHAR(400) NOT NULL,
-    [ImageUrl] NVARCHAR(50) NOT NULL,
+    [ImageUrl] NVARCHAR(200) NOT NULL,
 	[CategoryId] INT NOT NULL
 		CONSTRAINT FK_Products_ProductCategories FOREIGN KEY REFERENCES dbo.ProductCategories([Id]),
-	[Price] DECIMAL(10,2) NOT NULL
+	[Price] DECIMAL(10, 2) NOT NULL
         CONSTRAINT CH_Products_Price CHECK ([Price] > 0)
 );
 GO
