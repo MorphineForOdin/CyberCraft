a324abb9226a61ca39120a8d9a63d88d2614dfac
Yurii-Borys <leolux.avto@gmail.com>
2021-07-20 16:33:11 +0300

    fixed

--- src/TechMarket.WEB/tech-market/src/app/shared/slider/slider.component.ts
+++ src/TechMarket.WEB/tech-market/src/app/shared/slider/slider.component.ts
@@ -10,29 +10,25 @@ export class SliderComponent implements OnInit {
 
     @Input() images: SliderImage[] = <SliderImage[]>[];
 
-    constructor() { };
     i=0;
 
   getSlide() {
     return this.images[this.i].imageUrl;
-  }
+    }
 
   getSlideLink() {
     return this.images[this.i].linkUrl;
-  }
+    }
+
   getPrev() {
     this.i = this.i === 0 ? 0 : this.i - 1;
-  }
+    }
 
   getNext() {
     this.i = this.i === this.images.length - 1 ? this.i : this.i + 1;
-        }
+    }
 
   ngOnInit() {
     this.i = 0;
-  }
-
+    }
 }
-
-
-
