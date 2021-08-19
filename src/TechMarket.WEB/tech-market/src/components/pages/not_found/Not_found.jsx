fd202475d3eb215aed898876dba183826aef3a14
Yurii-Borys <leolux.avto@gmail.com>
2021-08-19 17:12:54 +0300

    fixed

--- src/TechMarket.WEB/tech-market/src/components/pages/not_found/Not_found.jsx
+++ src/TechMarket.WEB/tech-market/src/components/pages/not_found/Not_found.jsx
@@ -13,8 +13,8 @@ const Not_found = () => {
                     <h3 className="h2"> Look like you're lost</h3>
                     <p>the page you are looking for not avaible!</p>
                     <div className="row justify-content-center ">
-                        <div className="col-3 col-xl-1 button border border-dark text-center">
-                            <a href="/" className="text-decoration-none text-dark p-2" >Go home</a>
+                        <div className="col-3 col-md-2">
+                            <a href="/" className="btn btnNotFound border border-dark text-dark w-100 p-2" role="button">Go home</a>
                         </div>
                     </div>
                 </div>
