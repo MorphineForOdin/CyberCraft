270811041a0d485ee0735ae9f03f610271eb0d11
sviastyn <svyastynvasul@gmail.com>
2021-08-25 11:42:44 +0300

    Fixed.

--- src/TechMarket.WEB/tech-market/src/components/Slider/ImageSlider.jsx
+++ src/TechMarket.WEB/tech-market/src/components/Slider/ImageSlider.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
-import { ArrowRightCircle, ArrowLeftCircle } from 'react-bootstrap-icons';  
+import { ArrowRightCircle, ArrowLeftCircle } from 'react-bootstrap-icons';
 import './ImageSlider.css'
 
 const ImageSlider = ({ images }) => {
 
     const [current, setCurrent] = useState(0);
     const length = images.length;
-    
+
     const getNext = () => {
         setCurrent(current === length - 1 ? 0 : current + 1);
     };
 
     const getPrev = () => {
         setCurrent(current === 0 ? length - 1 : current - 1);
-    };   
+    };
 
     return (
         <div className="slide_container" style={{
--- src/TechMarket.WEB/tech-market/src/components/pages/home/Home.jsx
+++ src/TechMarket.WEB/tech-market/src/components/pages/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             <Container className='mt-3'>
                 <Row className='justify-content-between'>
                     <Col sm={12} md={2} lg={3} className='home_col'>
-                        <h4>Title#1</h4>
+                        <h4 className='text-center'>Title#1</h4>
                         <ul>
                             <li>Lorem</li>
                             <li>Lorem, ipsum dolor.</li>
@@ -23,18 +23,18 @@ const Home = () => {
                             <li>Lorem ipsum dolor, sit amet consectetur.</li>
                         </ul>
                     </Col >
-                    <Col sm={12} md={4} lg={4} className='home_col'>
-                        <h4>Title#2</h4>
+                    <Col sm={12} md={4} className='home_col'>
+                        <h4 className='text-center'>Title#2</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse repellendus, animi quaerat corrupti corporis harum eveniet distinctio, laudantium temporibus cumque ea, molestiae sint cum voluptates. Nam possimus assumenda consectetur odio.</p>
                     </Col>
                     <Col sm={12} md={5} lg={4} className='home_col'>
-                        <h4>Title#1</h4>
+                        <h4 className='text-center'>Title#3</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem veritatis vero minus nesciunt iste animi repellendus pariatur commodi sint, sapiente soluta quaerat exercitationem ea officiis totam esse dolor ipsa, quam ratione porro? Quis neque iste ullam blanditiis labore sint dolorum maxime perspiciatis maiores. Adipisci, quos.</p>
                     </Col>
                 </Row>
             </Container>
             <div className='mt-4'>
-                <h4>Products will be here soon!</h4>
+                <h4 className='text-center'>Products will be here soon!</h4>
             </div>
         </div>
     );
