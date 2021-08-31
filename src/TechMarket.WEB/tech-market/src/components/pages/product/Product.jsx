4caa155fec94b97484797fed24a8ff51fe2b4ae3
sviastyn <svyastynvasul@gmail.com>
2021-08-31 16:13:36 +0300

    Fixed.

--- src/TechMarket.WEB/tech-market/src/components/pages/product/Product.jsx
+++ src/TechMarket.WEB/tech-market/src/components/pages/product/Product.jsx
@@ -18,36 +18,33 @@ const Product = () => {
         <div>
             <Container>
                 <Row className='mt-3'>
-                    <Col sm={12} md={6}>
+                    <Col xs={12} md={6}>
                         <h3 className='text-center'>{prod.name}</h3>
                     </Col>
-                    <Col>
-                        {prod.quantity === 0 
-                            ? <span className='text-danger border border-danger p-1'>Expected</span> 
-                                : <span className='text-success border border-success p-1'>In stock</span>}
+                    <Col xs={6} md={4}>
+                        {prod.quantity === 0
+                            ? <span className='text-danger border border-danger p-1'>Expected</span>
+                            : <span className='text-success border border-success p-1'>In stock</span>}
                     </Col>
-                    <Col>
-                        <div className='text-end'>ID: {prod.id}</div>
+                    <Col xs={6} md={2} className='text-end'>
+                        <div>ID: {prod.id}</div>
                     </Col>
-                </Row>
-                <Row className='mt-3'>
-                    <Col className='d-flex justify-content-center' sm={12} md={6}>
+                    <Col className='d-flex justify-content-center mt-3' xs={12} md={6}>
                         <img width='80%' src={prod.imageUrl} alt='product' />
                     </Col>
-                    <Col sm={12} md={6}>
+                    <Col xs={12} md={6} className='mt-3'>
                         <h4>Price: ${prod.price}</h4>
                         <Row>
                             <Col sm={12} md={5}>
                                 <button type="button" className="btn btn-dark w-100">Buy</button>
                             </Col>
                         </Row>
-
                         <ul className='mt-2'>
                             <h4>Attributes:</h4>
                             <h5>{attributesList}</h5>
                         </ul>
                     </Col>
-                    <Col className='mt-3'>
+                    <Col xs={12} className='mt-2'>
                         <div>Description: {prod.description}</div>
                     </Col>
                 </Row>
